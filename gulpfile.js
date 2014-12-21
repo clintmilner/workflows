5c9df93fff76ae65111aa503c14b4309b9adca14
Clint Milner <clint@sympoz.com>
2014-12-21 16:01:57 +0000

    Installed Gulp and GUtil

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+/**
+ * Created by clint.milner on 12/21/14.
+ */
+var gulp = require( 'gulp' ),
+    gutil = require( 'gulp-util' );
+
+gulp.task( 'log', function(){
+    gutil.log( 'workflows are awesome!' );
+});
