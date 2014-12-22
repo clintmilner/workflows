e43869e027ccbddd9691b498b375e563dde4446f
Clint Milner <clint@sympoz.com>
2014-12-22 14:38:16 +0000

    Adding Sass compliling with Compass

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var gulp = require( 'gulp' ),
     gutil = require( 'gulp-util' ),
     coffee = require( 'gulp-coffee' ),
     browserify = require( 'gulp-browserify' ),
+    compass = require( 'gulp-compass' ),
     concat = require( 'gulp-concat' );
 
 var coffeeSources = [ 'components/coffee/tagline.coffee' ];
@@ -14,6 +15,7 @@ var jsSources = [
     'components/scripts/tagline.js',
     'components/scripts/template.js'
 ];
+var sassSources = [ 'components/sass/style.scss' ];
 
 gulp.task( 'coffee', function(){
    gulp.src( coffeeSources )
@@ -28,4 +30,17 @@ gulp.task( 'js', function(){
         .pipe( concat( 'script.js' ) )
         .pipe( browserify() )
         .pipe( gulp.dest( 'builds/development/js' ))
+});
+
+gulp.task( 'compass', function(){
+    gulp.src( sassSources )
+        .pipe( compass(
+            {
+                sass: 'components/sass',
+                image: 'builds/development/images',
+                style: 'expanded'
+            }
+        ))
+        .on( 'error', gutil.log )
+        .pipe( gulp.dest( 'builds/development/css' ))
 });
