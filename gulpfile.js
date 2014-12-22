e92ea95cf1ce66843695459d086114aedb803927
Clint Milner <clint@sympoz.com>
2014-12-22 13:53:31 +0000

    Added CoffeeScript files

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,15 @@
  * Created by clint.milner on 12/21/14.
  */
 var gulp = require( 'gulp' ),
-    gutil = require( 'gulp-util' );
+    gutil = require( 'gulp-util' ),
+    coffee = require( 'gulp-coffee' );
 
-gulp.task( 'log', function(){
-    gutil.log( 'workflows are awesome!' );
+var coffeeSources = [ 'components/coffee/tagline.coffee' ];
+
+gulp.task( 'coffee', function(){
+   gulp.src( coffeeSources )
+       .pipe( coffee( { bare: true } )
+           .on( 'error', gutil.log ) )
+       .pipe( gulp.dest( 'components/scripts' )
+   )
 });
